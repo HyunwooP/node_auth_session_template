83191e636453d26859c819e826a5c754a322a17f
harry <awakelife93@gmail.com>
2021-06-30 17:24:11 +0900

    health API - memory 체크 로직 추가

--- src/models/Common/service/index.ts
+++ src/models/Common/service/index.ts
@@ -1,3 +1,4 @@
+import * as os from "os";
 import {
   CommonStatusCode,
   CommonStatusMessage,
@@ -6,6 +7,18 @@ import {
 
 export const _health = async (): Promise<object> => {
   try {
+    const totalmem = os.totalmem();
+    const freemem = os.freemem();
+    const memPercent = (freemem / totalmem) * 100;
+    const memoryLimit = 90;
+
+    if (memPercent >= memoryLimit) {
+      onFailureHandler({
+        status: CommonStatusCode.INTERNAL_SERVER_ERROR,
+        message: `현재 메모리 점유율이 90% 이상입니다. - ${new Date().toISOString()}`,
+      });
+    }
+
     return {};
   } catch (e) {
     onFailureHandler({
