63ff67a42d6f1af23a32457e161bbf73cfab291c
harry <awakelife93@gmail.com>
2021-07-06 19:46:06 +0900

    게이트웨이 서버만 바라본다

--- src/config/index.ts
+++ src/config/index.ts
@@ -23,5 +23,5 @@ export default {
   origin:
     process.env.clientDomain && process.env.clientPort
       ? `http://${process.env.clientDomain}:${process.env.clientPort}`
-      : "http://localhost:3000",
+      : "http://localhost:8080",
 };
